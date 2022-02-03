268f3d127559575ee37639334acf1ec74db74b02
unknown <justlyjohn198@gmail.com>
2022-02-03 21:20:49 +0100

    Commiting changes

--- src/components/commons/PreLoader.js
+++ src/components/commons/PreLoader.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const PreLoader = () => {
   return (
     <div id="preloader">
-        <div className="sk-three-bounce">
-            <div className="sk-child sk-bounce1" />
-            <div className="sk-child sk-bounce2" />
-            <div className="sk-child sk-bounce3" />
-        </div>
+      <div className="sk-three-bounce">
+        <div className="sk-child sk-bounce1" />
+        <div className="sk-child sk-bounce2" />
+        <div className="sk-child sk-bounce3" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PreLoader
+export default PreLoader;

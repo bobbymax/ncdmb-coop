dd197d6bab5058f7a4ba037b496e0d56f1d5ba86
unknown <justlyjohn198@gmail.com>
2022-02-04 04:41:47 +0100

    changes made

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,11 +1,18 @@
 import { Formik } from "formik";
 
-const AppForm = ({ children, initialValues, onSubmit, validationSchema }) => {
+const AppForm = ({
+  children,
+  initialValues,
+  onSubmit,
+  validationSchema,
+  innerRef,
+}) => {
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
+      innerRef={innerRef}
     >
       {() => <>{children}</>}
     </Formik>
--- src/views/modules/structure/GradeLevels.js
+++ src/views/modules/structure/GradeLevels.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import BasicTable from "../../../components/commons/tables/BasicTable";
 import Form from "../../../components/forms/Form";
 import FormInput from "../../../components/forms/FormInput";
@@ -14,7 +14,6 @@ import {
   store,
   destroy,
 } from "../../../services/utils/controllers";
-import { validate } from "../../../services/utils/validation";
 import useApi from "../../../services/hooks/useApi";
 
 const GradeLevels = () => {
@@ -22,33 +21,14 @@ const GradeLevels = () => {
 
   useEffect(() => {
     request("gradeLevels");
-
-    // try {
-    //   collection("gradeLevels")
-    //     .then((res) => {
-    //       setRoles(res.data.data);
-    //     })
-    //     .catch((err) => console.log(err.message));
-    // } catch (error) {
-    //   console.log(error);
-    // }
   }, []);
 
-  const initialState = {
-    id: 0,
-    name: "",
-    max_slots: 0,
-    isSuper: 0,
-    start_date: "",
-    expiry_date: "",
-    cannot_expire: 0,
-  };
+  const initialStateRef = useRef();
 
   const [roles, setRoles] = useState([]);
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState(initialStateRef.current.values);
   const [update, setUpdate] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [errors, setErrors] = useState({});
+  const [open, setOpen] = useState(true);
   // const [status, setStatus] = useState(false)
 
   const columns = [
@@ -57,89 +37,60 @@ const GradeLevels = () => {
       key: "name",
     },
     {
-      label: "Slot",
-      key: "max_slots",
+      label: "Code",
+      key: "code",
     },
-  ];
-
-  const options = [
-    { key: "1", label: "Yes" },
-    { key: "0", label: "No" },
-  ];
-
-  const rules = [
-    { name: "name", rules: ["required", "string", "min:3"] },
-    { name: "max_slots", rules: ["required", "integer"] },
-    { name: "start_date", rules: ["required"] },
-    { name: "isSuper", rules: ["required"] },
-    { name: "cannot_expire", rules: ["required"] },
   ];
 
   const handleUpdate = (data) => {
     setState(data);
+    console.log(data);
     setUpdate(true);
     setOpen(true);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (data, { resetForm }) => {
+    if (update) {
+      try {
+        alter("gradeLevels", state.id, data)
+          .then((res) => {
+            const result = res.data.data;
 
-    const data = {
-      name: state.name,
-      max_slots: parseInt(state.max_slots),
-      isSuper: state.isSuper,
-      start_date: state.start_date,
-      expiry_date: state.expiry_date,
-      cannot_expire: state.cannot_expire,
-    };
+            setRoles(
+              roles.map((el) => {
+                if (result.id === el.id) {
+                  return result;
+                }
 
-    const formErrors = validate(rules, data);
-    setErrors(formErrors);
-    const status =
-      Object.keys(formErrors).length === 0 && formErrors.constructor === Object;
-
-    if (status) {
-      if (update) {
-        try {
-          alter("roles", state.id, data)
-            .then((res) => {
-              const result = res.data.data;
-
-              setRoles(
-                roles.map((el) => {
-                  if (result.id === el.id) {
-                    return result;
-                  }
-
-                  return el;
-                })
-              );
-              Alert.success("Updated", res.data.message);
-            })
-            .catch((err) => console.log(err.message));
-        } catch (error) {
-          console.log(error);
-        }
-      } else {
-        try {
-          store("roles", data)
-            .then((res) => {
-              const result = res.data.data;
-              setRoles([result, ...roles]);
-              Alert.success("Created!!", res.data.message);
-            })
-            .catch((err) => console.log(err.message));
-        } catch (error) {
-          console.log(error);
-        }
+                return el;
+              })
+            );
+            Alert.success("Updated", res.data.message);
+          })
+          .catch((err) => console.log(err.message));
+      } catch (error) {
+        console.log(error);
       }
+    } else {
+      try {
+        store("gradeLevels", data)
+          .then((res) => {
+            const result = res.data.data;
 
-      setErrors({});
-
-      setUpdate(false);
-      setState(initialState);
-      setOpen(false);
+            setRoles([result, ...roles]);
+            Alert.success("Created!!", res.data.message);
+          })
+          .catch((err) => console.log(err.message));
+      } catch (error) {
+        console.log(error);
+      }
     }
+
+    setUpdate(false);
+    setState(initialStateRef.current.values);
+    // setOpen(false);
+
+    resetForm();
   };
 
   const handleDestroy = (data) => {
@@ -179,149 +130,50 @@ const GradeLevels = () => {
             <div className="card">
               <div className="card-body">
                 <div className="form-body">
-                  <form onSubmit={handleSubmit}>
+                  <form onSubmit={(values) => console.log(values)}>
                     <Form
                       initialValues={{
-                        id: 0,
+                        code: "",
                         name: "",
-                        max_slots: 0,
-                        isSuper: 0,
-                        start_date: "",
-                        expiry_date: "",
-                        cannot_expire: 0,
                       }}
+                      innerRef={initialStateRef}
+                      o
+                      onSubmit={handleSubmit}
                     >
                       <div className="row">
-                        <div className="col-md-4">
-                          <TextInputField
-                            placeholder="Enter Role Name"
-                            value={state.name}
-                            onChange={(e) =>
-                              setState({ ...state, name: e.target.value })
-                            }
-                            error={
-                              errors && errors.name && errors.name.length > 0
-                            }
-                            errorMessage={
-                              errors && errors.name && errors.name[0]
-                            }
-                          />
-                        </div>
-                        <div className="col-md-4">
-                          <TextInputField
-                            placeholder="Enter Max Slot"
-                            type="number"
-                            value={state.max_slots}
-                            onChange={(e) =>
-                              setState({ ...state, max_slots: e.target.value })
-                            }
-                            error={
-                              errors &&
-                              errors.max_slots &&
-                              errors.max_slots.length > 0
-                            }
-                            errorMessage={
-                              errors && errors.max_slots && errors.max_slots[0]
-                            }
-                          />
-                        </div>
-                        <div className="col-md-4">
-                          <CustomSelect
-                            defaultText="Is Role Admin?"
-                            options={options}
-                            value={state.isSuper}
-                            onChange={(e) =>
-                              setState({ ...state, isSuper: e.target.value })
-                            }
-                            error={
-                              errors &&
-                              errors.isSuper &&
-                              errors.isSuper.length > 0
-                            }
-                            errorMessage={
-                              errors && errors.isSuper && errors.isSuper[0]
-                            }
-                          />
-                        </div>
-                        <div className="col-md-4">
-                          <TextInputField
-                            placeholder="Start Date"
-                            type="date"
-                            value={state.start_date}
-                            onChange={(e) =>
-                              setState({ ...state, start_date: e.target.value })
-                            }
-                            error={
-                              errors &&
-                              errors.start_date &&
-                              errors.start_date.length > 0
-                            }
-                            errorMessage={
-                              errors &&
-                              errors.start_date &&
-                              errors.start_date[0]
-                            }
+                        <div className="col-md-6">
+                          <FormInput
+                            name="name"
+                            placeholder="Grade Level Name"
                           />
                         </div>
 
-                        <div className="col-md-4">
-                          <TextInputField
-                            placeholder="Expiry Date"
-                            type="date"
-                            value={state.expiry_date}
-                            onChange={(e) =>
-                              setState({
-                                ...state,
-                                expiry_date: e.target.value,
-                              })
-                            }
+                        <div className="col-md-6">
+                          <FormInput
+                            name="code"
+                            placeholder="Grade Level Code"
                           />
                         </div>
 
-                        <div className="col-md-4">
-                          <CustomSelect
-                            defaultText="Cannot Expire?"
-                            options={options}
-                            value={state.cannot_expire}
-                            onChange={(e) =>
-                              setState({
-                                ...state,
-                                cannot_expire: e.target.value,
-                              })
-                            }
-                            error={
-                              errors &&
-                              errors.cannot_expire &&
-                              errors.cannot_expire.length > 0
-                            }
-                            errorMessage={
-                              errors &&
-                              errors.cannot_expire &&
-                              errors.cannot_expire[0]
-                            }
-                          />
-                        </div>
-
-                        <div className="col-md-12 mt-3">
-                          <button type="submit" className="btn btn-primary">
+                        <div className="col-md-12 mt-3 d-flex">
+                          <SubmitButton title="Submit" />
+                          {/* <button type="submit" className="btn btn-primary">
                             Submit
-                          </button>
+                          </button> */}
+
                           <button
                             type="button"
                             className="btn btn-danger"
                             onClick={() => {
                               setUpdate(false);
-                              setState(initialState);
+                              setState(initialStateRef.current.values);
                               setOpen(false);
-                              setErrors({});
                             }}
                           >
                             Close
                           </button>
                         </div>
                       </div>
-
-                      {/* <SubmitButton /> */}
                     </Form>
                   </form>
                 </div>
@@ -335,9 +187,9 @@ const GradeLevels = () => {
         <BasicTable
           page="Roles"
           columns={columns}
-          rows={roles}
+          rows={gradeLevels}
           handleEdit={handleUpdate}
-          handleDelete={handleDestroy}
+          // handleDelete={handleDestroy}
         />
       </div>
     </div>

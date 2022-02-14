f2412b5439e18930267678741a0dc0ee1e61da5a
unknown <justlyjohn198@gmail.com>
2022-02-14 16:44:56 +0100

    Pushing changes towards closing

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import ExportToCsv from "./ExportToCsv";
 
 ReactDOM.render(
-  <ExportToCsv />,
-  // <Provider store={store}>
-  //   <Router>
-  //     <App />
-  //   </Router>
-  // </Provider>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );

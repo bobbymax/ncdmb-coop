fd2183c3d83acbb69defc8ce35ad5a2a31e8de66
unknown <justlyjohn198@gmail.com>
2022-02-18 20:39:45 +0100

    push

--- src/views/modules/administration/AssignRole.js
+++ src/views/modules/administration/AssignRole.js
@@ -1,13 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import useApi from "../../../services/hooks/useApi";
+import { collection } from "../../../services/utils/controllers";
+// import TextInputField from '../../'
 
 const AssignRole = () => {
-  useEffect(() => {}, []);
+  const { data: staffs, request } = useApi(collection);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    request("users");
+  }, []);
 
   return (
     <div className="row">
       <div className="col-md-12">
         <div className="page-titles">
           <h2>Assign Role</h2>
+        </div>
+      </div>
+
+      {open && <h2>Open</h2>}
+
+      <div className="col-md-12">
+        <div className="card">
+          <div className="card-body table-responsive">
+            <table className="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>Staff No</td>
+                  <td>Level</td>
+                  <td>Manage</td>
+                </tr>
+              </thead>
+
+              <tbody>
+                {staffs.length && staffs.length > 0 ? (
+                  staffs.map((staff) => (
+                    <tr key={staff.id}>
+                      <td>{staff.name}</td>
+                      <td>{staff.staff_no}</td>
+                      <td>{staff.level}</td>
+                      <td>
+                        <i className="fa fa-edit"></i>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={4}>No Data</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>

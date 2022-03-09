aa8364e9b0b730e151db967daeb82b1d7c99db84
unknown <justlyjohn198@gmail.com>
2022-03-09 16:29:28 +0100

    About to add profile

--- src/components/layout/partials/Aside.js
+++ src/components/layout/partials/Aside.js
@@ -141,6 +141,80 @@ const Aside = () => {
           </span>
         </div>
       </div>
+
+      <div className="aside-menu flex-column-fluid"></div>
+      <div
+        className="hover-scroll-overlay-y my-5 my-lg-5"
+        id="kt_aside_menu_wrapper"
+        data-kt-scroll="true"
+        data-kt-scroll-activate="{default: false, lg: true}"
+        data-kt-scroll-height="auto"
+        data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer"
+        data-kt-scroll-wrappers="#kt_aside_menu"
+        data-kt-scroll-offset="0"
+      >
+        <div
+          className="menu menu-column menu-title-gray-800 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500"
+          id="#kt_aside_menu"
+          data-kt-menu="true"
+          data-kt-menu-expand="false"
+        >
+          <div data-kt-menu-trigger="click" className="menu-item here">
+            <span className="menu-link">
+              <span className="menu-icon">
+                <span className="svg-icon svg-icon-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <rect
+                      x="2"
+                      y="2"
+                      width="9"
+                      height="9"
+                      rx="2"
+                      fill="black"
+                    />
+                    <rect
+                      opacity="0.3"
+                      x="13"
+                      y="2"
+                      width="9"
+                      height="9"
+                      rx="2"
+                      fill="black"
+                    />
+                    <rect
+                      opacity="0.3"
+                      x="13"
+                      y="13"
+                      width="9"
+                      height="9"
+                      rx="2"
+                      fill="black"
+                    />
+                    <rect
+                      opacity="0.3"
+                      x="2"
+                      y="13"
+                      width="9"
+                      height="9"
+                      rx="2"
+                      fill="black"
+                    />
+                  </svg>
+                </span>
+              </span>
+
+              <span className="menu-title">Dashboards</span>
+              <span className="menu-arrow"></span>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/layout/partials/Header.js
+++ src/components/layout/partials/Header.js
@@ -342,10 +342,10 @@ const Header = () => {
                           id="kt_header_user_menu_toggle"
                         >
                           <div
-                            className="cursor-pointer symbol symbol-30px symbol-md-40px"
+                            className="cursor-pointer symbol symbol-30px symbol-md-40px show menu-dropdown"
                             data-kt-menu-trigger="click"
                             data-kt-menu-attach="parent"
-                            data-kt-menu-placement="bottom-end"
+                            data-kt-menu-placement="bottom-start"
                           >
                             <img
                               src={
@@ -376,6 +376,7 @@ const Header = () => {
                                       Pro
                                     </span>
                                   </div>
+
                                   <a
                                     href="#"
                                     className="fw-bold text-muted text-hover-primary fs-7"
@@ -397,127 +398,7 @@ const Header = () => {
                               </a>
                             </div>
 
-                            <div className="menu-item px-5">
-                              <a
-                                href="../../demo1/dist/account/statements.html"
-                                className="menu-link px-5"
-                              >
-                                My Statements
-                              </a>
-                            </div>
-
                             <div className="separator my-2"></div>
-
-                            <div
-                              className="menu-item px-5"
-                              data-kt-menu-trigger="hover"
-                              data-kt-menu-placement="left-start"
-                            >
-                              <a href="#" className="menu-link px-5">
-                                <span className="menu-title position-relative">
-                                  Language
-                                  <span className="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">
-                                    English
-                                    <img
-                                      className="w-15px h-15px rounded-1 ms-2"
-                                      src="assets/media/flags/united-states.svg"
-                                      alt=""
-                                    />
-                                  </span>
-                                </span>
-                              </a>
-
-                              <div className="menu-sub menu-sub-dropdown w-175px py-4">
-                                <div className="menu-item px-3">
-                                  <a
-                                    href="../../demo1/dist/account/settings.html"
-                                    className="menu-link d-flex px-5 active"
-                                  >
-                                    <span className="symbol symbol-20px me-4">
-                                      <img
-                                        className="rounded-1"
-                                        src="assets/media/flags/united-states.svg"
-                                        alt=""
-                                      />
-                                    </span>
-                                    English
-                                  </a>
-                                </div>
-
-                                <div className="menu-item px-3">
-                                  <a
-                                    href="../../demo1/dist/account/settings.html"
-                                    className="menu-link d-flex px-5"
-                                  >
-                                    <span className="symbol symbol-20px me-4">
-                                      <img
-                                        className="rounded-1"
-                                        src="assets/media/flags/spain.svg"
-                                        alt=""
-                                      />
-                                    </span>
-                                    Spanish
-                                  </a>
-                                </div>
-
-                                <div className="menu-item px-3">
-                                  <a
-                                    href="../../demo1/dist/account/settings.html"
-                                    className="menu-link d-flex px-5"
-                                  >
-                                    <span className="symbol symbol-20px me-4">
-                                      <img
-                                        className="rounded-1"
-                                        src="assets/media/flags/germany.svg"
-                                        alt=""
-                                      />
-                                    </span>
-                                    German
-                                  </a>
-                                </div>
-
-                                <div className="menu-item px-3">
-                                  <a
-                                    href="../../demo1/dist/account/settings.html"
-                                    className="menu-link d-flex px-5"
-                                  >
-                                    <span className="symbol symbol-20px me-4">
-                                      <img
-                                        className="rounded-1"
-                                        src="assets/media/flags/japan.svg"
-                                        alt=""
-                                      />
-                                    </span>
-                                    Japanese
-                                  </a>
-                                </div>
-
-                                <div className="menu-item px-3">
-                                  <a
-                                    href="../../demo1/dist/account/settings.html"
-                                    className="menu-link d-flex px-5"
-                                  >
-                                    <span className="symbol symbol-20px me-4">
-                                      <img
-                                        className="rounded-1"
-                                        src="assets/media/flags/france.svg"
-                                        alt=""
-                                      />
-                                    </span>
-                                    French
-                                  </a>
-                                </div>
-                              </div>
-                            </div>
-
-                            <div className="menu-item px-5 my-1">
-                              <a
-                                href="../../demo1/dist/account/settings.html"
-                                className="menu-link px-5"
-                              >
-                                Account Settings
-                              </a>
-                            </div>
 
                             <div className="menu-item px-5">
                               <a
@@ -526,30 +407,6 @@ const Header = () => {
                               >
                                 Sign Out
                               </a>
-                            </div>
-
-                            <div className="separator my-2"></div>
-
-                            <div className="menu-item px-5">
-                              <div className="menu-content px-5">
-                                <label
-                                  className="form-check form-switch form-check-custom form-check-solid pulse pulse-success"
-                                  for="kt_user_menu_dark_mode_toggle"
-                                >
-                                  <input
-                                    className="form-check-input w-30px h-20px"
-                                    type="checkbox"
-                                    value="1"
-                                    name="mode"
-                                    id="kt_user_menu_dark_mode_toggle"
-                                    data-kt-url="../../demo1/dist/index.html"
-                                  />
-                                  <span className="pulse-ring ms-n1"></span>
-                                  <span className="form-check-label text-gray-600 fs-7">
-                                    Dark Mode
-                                  </span>
-                                </label>
-                              </div>
                             </div>
                           </div>
                         </div>
